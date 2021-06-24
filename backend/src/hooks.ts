b6c988588efe22ae0a6b46ffc86245f33ca0f43c
Philip Oesterlin <philip.oesterlin@gmail.com>
2021-06-24 23:10:45 +0200

    remove authentification

--- backend/src/hooks.ts
+++ backend/src/hooks.ts
@@ -9,31 +9,31 @@ const allowedPaths = ['/login', '/login/', '/login/undefined', '/api/testpin', '
 export const handle: Handle = async ({ request, resolve }) => {
 	const cookies = cookie.parse(request.headers.cookie || '');
 
-	const isInternal = request.headers.internal === iH().headers.internal;
+	// const isInternal = request.headers.internal === iH().headers.internal;
 
-	console.log(request.headers.internal, "=", iH().headers.internal)
-	console.log('internal:', isInternal, 'comparison', JSON.stringify({ is: cookies.pin, should: pin }), 'path:', request.path);
-	console.log("allowed anyways:", allowedPaths.includes(request.path))
-	console.log('check:', cookies.pin === pin)
+	// console.log(request.headers.internal, "=", iH().headers.internal)
+	// console.log('internal:', isInternal, 'comparison', JSON.stringify({ is: cookies.pin, should: pin }), 'path:', request.path);
+	// console.log("allowed anyways:", allowedPaths.includes(request.path))
+	// console.log('check:', cookies.pin === pin)
 
 
-	if (!isInternal && (cookies.pin !== pin || !allowedPaths.includes(request.path))) {
-		const host = request.headers.host;
+	// if (!isInternal && (cookies.pin !== pin || !allowedPaths.includes(request.path))) {
+	// 	const host = request.headers.host;
 
-		if (request.path.startsWith('/api/proxy')) {
-			console.log(request)
-			console.log("api call")
-			return { status: 404, body: JSON.stringify({ message: "unauthorized" }), headers: {} };
-		}
+	// 	if (request.path.startsWith('/api/proxy')) {
+	// 		console.log(request)
+	// 		console.log("api call")
+	// 		return { status: 404, body: JSON.stringify({ message: "unauthorized" }), headers: {} };
+	// 	}
 
-		console.log("redirect")
-		return {
-			status: 302,
-			headers: {
-				Location: (dev ? 'http://' : 'https://') + host + '/login'
-			}
-		};
-	}
+	// 	console.log("redirect")
+	// 	return {
+	// 		status: 302,
+	// 		headers: {
+	// 			Location: (dev ? 'http://' : 'https://') + host + '/login'
+	// 		}
+	// 	};
+	// }
 
 	console.log("------- stay ------")
 
