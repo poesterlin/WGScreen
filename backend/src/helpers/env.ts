aca4b77a7fc4dc38e164bc79a9dc9ae1227394a7
Philip Oesterlin <philip.oesterlin@gmail.com>
2021-07-31 13:54:47 +0200

    remove youtube integration

--- backend/src/helpers/env.ts
+++ backend/src/helpers/env.ts
@@ -8,8 +8,6 @@ export const dbServer = env.VITE_DBSERVER ?? `http://${base}:1337/`;
 export const pw = env.VITE_PW ?? 'EMs2UMRq8S4hsfn';
 export const user = env.VITE_USER ?? 'philip.oesterlin@gmail.com';
 export const pin = env.VITE_PIN ?? '1234';
-export const YoutubeToken = env.VITE_YOUTUBETOKEN;
-export const YoutubeClient = env.VITE_YOUTUBECLIENT;
 
 export function iH() {
     return { headers: { internal: '9b3cb6de-ebfc-4116-93b3-733cc5880816' } };
